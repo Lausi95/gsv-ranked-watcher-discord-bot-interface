5523372e5959d190534725f8d17ecab03c119400
Tom Lausmann <tomlausmann@gmail.com>
2023-12-21 22:32:32 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,15 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
   implementation("org.springframework.kafka:spring-kafka")
+
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+  implementation("org.jetbrains.kotlin:kotlin-reflect")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0-RC")
+
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
   testImplementation("org.springframework.boot:spring-boot-starter-test")
   testImplementation("org.springframework.kafka:spring-kafka-test")
