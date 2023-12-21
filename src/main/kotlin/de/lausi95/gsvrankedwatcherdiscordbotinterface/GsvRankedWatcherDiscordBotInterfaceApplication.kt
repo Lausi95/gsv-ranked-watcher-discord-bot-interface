82506f02038ceffc0816601058cd5187bc7ff257
Tom Lausmann <tomlausmann@gmail.com>
2023-12-21 22:29:05 +0100

    Initialize Spring Boot project

---
+++ src/main/kotlin/de/lausi95/gsvrankedwatcherdiscordbotinterface/GsvRankedWatcherDiscordBotInterfaceApplication.kt
@@ -0,0 +1,11 @@
+package de.lausi95.gsvrankedwatcherdiscordbotinterface
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GsvRankedWatcherDiscordBotInterfaceApplication
+
+fun main(args: Array<String>) {
+  runApplication<GsvRankedWatcherDiscordBotInterfaceApplication>(*args)
+}
